0972037e8b591087f2bbd2f495ad2427aa813bfe
zhongming Fan <zhongmingfan@canway.net>
2023-09-28 16:47:47 +0800

    dp: cluster

--- weops/pipe-tools/exporter/template/build.sh
+++ weops/pipe-tools/exporter/template/build.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 for version in v0-10-0-2 v0-11-0-3 v1-0-2 v2-0-1 v2-1-0; do
+    # standalone
     standalone_output_file="standalone_${version}.yaml"
     string_version=$(echo "$version" | tr '-' '.' | cut -c 2-)
     sed "s/{{VERSION}}/${version}/g; s/{{STRING_VERSION}}/${string_version}/g" standalone.tpl > ../standalone/${standalone_output_file}
 done
+
+version="3.5.1"
+# cluster
+cluster_output_file="cluster_${version}.yaml"
+string_version=$(echo "$version" | tr '-' '.' | cut -c 2-)
+sed "s/{{VERSION}}/${version}/g; s/{{STRING_VERSION}}/${string_version}/g" cluster.tpl > ../cluster/${standalone_output_file}
 
